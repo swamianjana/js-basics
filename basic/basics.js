3de1608a7548ed5ece210341fc4b9f98af6fe8fc
Anjana Swami <anjana20swami@gmail.com>
2022-05-24 18:18:08 +0530

    Learn variables, keywords.

---
+++ basic/basics.js
@@ -0,0 +1,41 @@
+// keywords: predefined;
+// let, const, if, var, function, for, return, switch .... etc.
+
+// variables : it is name for keyword
+// inisilization, decalration
+
+// var is variable to store some data
+var a;
+// var = keyword 
+// a = variable
+console.log("Hello naveen 123");
+
+console.log( 60 + 5);
+console.log(60 + 10);
+
+var n1 = 10; // numeric
+var n2 = 6; // numeric
+var n3 = 2; // numeric
+
+console.log(n1 + n2 +  n3);
+console.log(n1 + n3 );
+
+var sum = n1 + n2 + n3;
+console.log(sum);
+
+console.log("sum :  ", sum);
+
+console.log("multiply of two number: ", n1 * n2);
+
+var mul = n1 * n3;
+console.log("multiply: ", mul);
+
+
+var name = "Anjana Swami"; // string
+console.log("abc ", name );
+
+var sub  = n1 - n2;
+console.log(sub);
+
+var div  = n1 / n2;
+console.log(div);
