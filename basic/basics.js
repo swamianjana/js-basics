14b9451b03f82034b87994accbe618b21c1c38e9
Anjana Swami <anjana20swami@gmail.com>
2022-05-28 21:59:27 +0530

    learn var,let ,const,....

--- basic/basics.js
+++ basic/basics.js
@@ -10,6 +10,7 @@ var a;
 // a = variable
 console.log("Hello naveen 123");
 
+// opertors ( +, -, *, / , %)
 console.log( 60 + 5);
 console.log(60 + 10);
 
@@ -33,9 +34,58 @@ console.log("multiply: ", mul);
 
 var name = "Anjana Swami"; // string
 console.log("abc ", name );
-
 var sub  = n1 - n2;
 console.log(sub);
 
 var div  = n1 / n2;
 console.log(div);
+
+// x = 5 and y = 6
+// multiply of two number x and y is equal to: 30
+
+var x = 6;
+var y = 6;
+var multiply = x * y;
+console.log("multiply: ", multiply);
+console.log("multiply of two number x and y is equal to: ", x * y); 
+
+// Reminder..... %
+
+var num1 = 3;
+var num2 = 2;
+var rem = num1 % num2;
+console.log(" Reminder of 5 and 2 is :   ", rem);
+
+// concated: Add two strings ...
+
+// you have first name and last name and you have to add them and beome full name
+
+var firstName = "Anjana"; // string
+var lastName =  "Swami"; // string
+
+// you have to made full name  ( Anjana Swami )
+
+console.log(" Full Name is:  " ,firstName + " " + lastName);
+        //    "Anjana "      "Swami"   
+var fullName = firstName + " " + lastName; //fullName = anjana swami
+
+console.log("fullName 1: ", fullName);
+
+console.log(5 +6);
+console.log("naveen" + 5);
+console.log("Anjana "+ "swami");
+console.log(10 + " Swami");
+console.log("Anjana " + 10+ " Swami");
+console.log("Swami " + 5 + 5);
+console.log(5 + 5 + " Swami");
+
+// Sum of num1 and num2 is equal to
+console.log("Sum of 5 and 2 is equal to", num1 + num2); // static : fixed string/ interger....
+
+// dynamic: change as per variable
+console.log("Sum of "+ num1 + " and " + num2 + " is equal to ", num1+ num2) ;
+// "Sum of 6 and 2 is equal to 8";
+
+console.log("naveen " + (10 + 10)); 
+console.log("naveen " + 10 + (10 - 10 + 5));
+console.log("naveen "+ 10 + (10 - 10));
---
+++ basic/variable.js
@@ -0,0 +1,131 @@
+// varaibles
+// var, let, const, use nothing
+
+// var
+var a = 5;
+console.log(a);
+
+// let
+let b = 5
+console.log(b);
+
+// const
+const c = 5;
+console.log(c);
+
+// nothing == var
+d = 5;
+console.log(d);
+
+// Explain var
+// var = variable
+// name = identifier
+// "Anjana" = value
+// data type = string
+// var fName = "Anjana"; // decalaration and assign together
+var fName; // decalration
+console.log( fName);
+fName = "Anjana"; // assign
+console.log("Name: ", fName);
+fName = "Naveen"; // re assing
+console.log("Name ", fName);
+
+
+// explain let
+let lName; // value = undefined
+console.log("Last name ", lName);
+lName = "Kumari"; // value = kumari
+console.log("Last name ", lName);
+lName = "Swami"; // value =swami
+console.log("Last Name ", lName);
+
+// explain const: constant means it Never changes
+// const mName; // can't decalaration only
+const mName = "Kumar"; // decaration and assign together
+console.log("Middle Name", mName);
+// mName = "kumari"; // throw error -> can't change value of constant variable
+console.log("middle Name", mName);
+
+// let x = 5;
+// let y = 6;
+// let z = 7;
+
+let x = 5, y = 6, z = 7; // same as above code
+
+// difference 2
+// Re-declaring variables;
+var carName = "i20"; // value =i20
+console.log(carName);
+var carName; // re - declare // value = it will take preious value if current value is undefined
+console.log("carName ", carName); // i20
+
+var carName = "fortunure"; // re-declare
+console.log("Car Name ", carName);
+
+let phone = "Realme";
+console.log(phone);
+phone= "iPhone"; // re-assing
+// let phone; // can't re-decalre
+// let phone = "sumsung";
+console.log("Phone", phone);
+
+// var drawbacks: we can re-declare
+// line no 20
+var laptop = "Macbook";
+
+// line no 200
+var laptop =  "Dell";
+console.log("Laptop ", laptop);
+
+//  for let
+let book = "English"; // line no at 50
+// line no at 150
+book = "Math"
+console.log("BooK", book);
+
+// const uses;
+// for constant value
+const pi = 22/7;
+const carTyreSize = 4;
+// pi = 44;
+console.log(pi); 
+
+var abc = "Anjana"
+console.log(abc);
+
+abc = "Swami" // re-assing
+
+console.log(abc);
+
+var abc = "Naveen";
+console.log(abc);
+
+// let abc = "Naveen Swani"; // re-decalring // not allowed
+console.log(abc);
+
+
+let xyz = "Person";
+console.log(xyz);
+
+// var xyz = "Animal"; // not allowed
+// console.log(xyz);
+
+
+// #######################################
+
+
+
+// BLOCK SCOPE
+// variable declare iside this{} cannot asses
+// EXAMPLE : 1
+// {
+//     let x = 5;
+// }
+// x cannot be used here
+
+// EXAMPLE :2
+// {
+//     var x = 5;
+
+// }
+// x can be used;
